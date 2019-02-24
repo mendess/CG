a75d1473bd4a4fb2c2e539426bb8822e008177e0
Mendess2526 <pedro.mendes.26@gmail.com>
2019-02-24 02:27:01 +0000

    Refactor

--- src/common/point.hpp
+++ src/common/point.hpp
@@ -1,20 +1,20 @@
 #ifndef POINT_H
 #define POINT_H
-#include <string>
-#include <sstream>
 #include <math.h>
+#include <sstream>
+#include <string>
 
 class Point {
-    private:
-        float _x,_y,_z;
-    public:
-        Point();
-        Point(float, float, float);
-        std::string toString();
-        float x(){ return _x; }
-        float y(){ return _y; }
-        float z(){ return _z; }
+private:
+    float _x, _y, _z;
+
+public:
+    Point();
+    Point(float, float, float);
+    std::string toString();
+    float x() { return _x; }
+    float y() { return _y; }
+    float z() { return _z; }
 };
 
-
-#endif// POINT_H
+#endif // POINT_H
--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,13 +1,5 @@
-#include "../common/point.hpp"
-#include "../dependencies/rapidxml.hpp"
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-#include <functional>
+#include "model.hpp"
 #include <iostream>
-#include <random>
-#include <string>
-#include <vector>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -16,10 +8,7 @@
 
 #include <math.h>
 
-using namespace rapidxml;
 using namespace std;
-
-static vector<vector<Point*>> models;
 
 float CAM_D = 5;
 float SCALE = 1;
@@ -52,92 +41,21 @@ void changeSize(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
-char* read_file()
-{
-    char* file_contents;
-    FILE* input_file = fopen("config.xml", "rb");
-    if (input_file == NULL) {
-        return NULL;
-    }
-    fseek(input_file, 0, SEEK_END);
-    long input_file_size = ftell(input_file);
-    rewind(input_file);
-    file_contents = (char*)malloc(input_file_size * (sizeof(char)));
-    fread(file_contents, sizeof(char), input_file_size, input_file);
-    fclose(input_file);
-    return file_contents;
-}
-
-void drawTriangle(Point* a, Point* b, Point* c)
-{
-    static default_random_engine generator;
-    static uniform_int_distribution<int> distribution(0, 100);
-    static auto rng = std::bind(distribution, generator);
-    float r = (((float)rng()) / 100.0f);
-    float g = (((float)rng()) / 100.0f);
-    float u = (((float)rng()) / 100.0f);
-    glColor3f(r, g, u);
-    glBegin(GL_TRIANGLES);
-    {
-        glVertex3f(a->x(), a->y(), a->z());
-        glVertex3f(b->x(), b->y(), b->z());
-        glVertex3f(c->x(), c->y(), c->z());
-    }
-    glEnd();
-}
-
-vector<Point*> loadModel(char* modelFile)
-{
-    float x, y, z;
-    vector<Point*> model;
-    ifstream infile(modelFile);
-    while (infile >> x >> y >> z) {
-        model.push_back(new Point(x, y, z));
-    }
-
-    return model;
-}
-
-void drawModel(vector<Point*> model)
-{
-    vector<Point*>::iterator it = model.begin();
-    while (it != model.end()) {
-        Point* a = *it++;
-        Point* b = *it++;
-        Point* c = *it++;
-        drawTriangle(a, b, c);
-    }
-}
-
 void renderScene()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(CAM_X, CAM_Y, CAM_Z,
-        0.0, 0.0, 0.0,
-        0.0f, 1.0f, 0.0f);
+    gluLookAt(CAM_X, CAM_Y, CAM_Z, 0.0, 0.0, 0.0, 0.0f, 1.0f, 0.0f);
     glScalef(SCALE, SCALE, SCALE);
-    for (vector<vector<Point*>>::iterator it = models.begin(); it != models.end(); ++it) {
-        drawModel(*it);
-    }
 
+    Models* models = Models::get();
+    for (int i = 0; i < models->num_models(); ++i) {
+        const Model* m = models->get_model(i);
+        if (!m->draw_model()) {
+            cerr << m->name() << ": Invalid number of control points" << endl;
+        }
+    }
     glutSwapBuffers();
-}
-
-void loadModels()
-{
-    char* text = read_file();
-    if (text == NULL) {
-        printf("config.xml file not found");
-        return;
-    }
-    xml_document<> doc;
-    doc.parse<0>(text);
-    xml_node<>* node = doc.first_node("scene");
-    for (node = node->first_node(); node; node = node->next_sibling()) {
-        xml_attribute<>* attr = node->first_attribute();
-        models.push_back(loadModel(attr->value()));
-    }
 }
 
 void key_bindings(unsigned char key, int _x, int _y)
@@ -188,7 +106,7 @@ void key_bindings(unsigned char key, int _x, int _y)
 
 int main(int argc, char** argv)
 {
-    loadModels();
+    Models::load("config.xml");
     // init GLUT and the window
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
