19a64c4d3119f2c56710c7dacf878491b2edb9d5
dunnadan <marcio.sousa98@hotmail.com>
2019-02-25 03:34:29 +0000

    Generator (plane and sphere)

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -1,4 +1,7 @@
 #include "generator.hpp"
+//#include <cstdlib>
+//#include <point.hpp>
+//#include <math.h>
 
 using namespace std;
 
@@ -17,3 +20,68 @@ vector<Point> draw_sphere(double radius, double slices, double stacks){
 vector<Point> draw_cone(double radius, double slices, double stacks){
     return vector<Point>();
 }
+
+
+//create vector to store the points (coords)
+
+void planeCoords(float sideLength){ 
+	
+	float axis = sideLength/2;
+
+	//first triangle
+	coords.push_back(new Point(axis, 0, axis));
+	coords.push_back(new Point(-axis, 0, axis));
+	coords.push_back(new Point(-axis, 0, -axis));
+
+	//second triangle
+	coords.push_back(new Point(-axis, 0, -axis));
+	coords.push_back(new Point(axis, 0, -axis));
+	coords.push_back(new Point(axis, 0, axis));
+}
+
+
+void sphereCoords(float radius, int stacks, int slices){
+
+	//calculate the Phi Movement (triangle's up/down side length) 
+	//PI * 2 because it is necessary to go all around to count all of the slices 
+	float phiMove = M_PI * 2 / slices;
+	
+	//calculate the Theta Movement (triangle's left/right side length)
+	//only PI because it is only necessary to go half the perimeter in the 
+	//vertical direction to count all of the stacks;
+	float thetaMove = M_PI / stacks;
+
+	
+	for(float phi = 0; phi < slices; phi++)
+		for(float theta = 0; theta < stacks; theta++){
+			
+			float currentStack = theta * thetaMove;
+			float currentSlice = phi * phiMove;
+
+			coords.push_back(new Point(radius * sin(currentStack + thetaMovement) * sin(currentSlice + phiMovement),
+						   radius * cos(currentStack + thetaMovement),
+						   radius * sin(currentStack + thetaMovement) * cos(currentSlice + phiMovement))); 
+			
+			coords.push_back(new Point(radius * sin(currentStack) * sin(currentSlice),
+                                                   radius * cos(currentStack),
+                                                   radius * sin(currentStack) * cos(currentSlice)));
+
+			coords.push_back(new Point(radius * sin(currentStack + thetaMovement) * sin(currentSlice),
+                                                   radius * cos(currentStack + thetaMovement),
+                                                   radius * sin(currentStack + thetaMovement) * cos(currentSlice)));
+
+			coords.push_back(new Point(radius * sin(currentStack) * sin(currentSlice + phiMovement),
+                                                   radius * cos(currentStack),
+                                                   radius * sin(currentStack) * cos(currentSlice + phiMovement)));
+
+			coords.push_back(new Point(radius * sin(currentStack) * sin(currentSlice),
+                                                   radius * cos(currentStack),
+                                                   radius * sin(currentStack) * cos(currentSlice)));
+
+			coords.push_back(new Point(radius * sin(currentStack + thetaMovement) * sin(currentSlice + phiMovement),
+                                                   radius * cos(currentStack + thetaMovement),
+                                                   radius * sin(currentStack + thetaMovement) * cos(currentSlice + phiMovement)));
+		}
+}
+
+
