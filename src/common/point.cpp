16f46402a08816b9fb4d8bf8dd707e3831ff904a
Mendess2526 <pedro.mendes.26@gmail.com>
2019-02-28 01:02:54 +0000

    Fix parameter parsing

--- src/common/point.cpp
+++ src/common/point.cpp
@@ -7,6 +7,6 @@ Point::Point(float x, float y, float z): _x(x), _y(y), _z(z){}
 std::string Point::to_string() const
 {
     std::ostringstream sstream;
-    sstream << _x << ' ' << _y << ' ' << _z << '\n';
+    sstream << _x << ' ' << _y << ' ' << _z;
     return sstream.str();
 }
