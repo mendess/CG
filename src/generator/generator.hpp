331cacb765f1fcf08c02cd81543fc4b8ed7231f9
Mendess2526 <pedro.mendes.26@gmail.com>
2019-03-01 18:44:15 +0000

    Add header file guard to generator

--- src/generator/generator.hpp
+++ src/generator/generator.hpp
@@ -1,3 +1,5 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
 #include "../common/point.hpp"
 #include <vector>
 
@@ -8,3 +10,5 @@ std::vector<Point*> draw_box(double x, double y, double z, int divisions);
 std::vector<Point*> draw_sphere(double radius, double slices, double stacks);
 
 std::vector<Point*> draw_cone(double radius, double height, int slices, int stacks);
+
+#endif // GENERATOR_H
