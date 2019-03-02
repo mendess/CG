821cddb135eb03490a601c99881cfe174cef0f50
Márcio Sousa <30596292+dunnadan@users.noreply.github.com>
2019-03-02 15:12:29 +0000

    Generator (plane and sphere) (#3)
    * Generator (plane and sphere)
    
    * Update generator(plane(done), box(done), sphere(done), cone(almost finished))
    
    * Updated generator (errors)
    
    * Fix function signatures
    
    * Fix parameter parsing
    
    * fix plane
    
    * cone adjustment
    
    * Fix parameter parsing
    
    * Cone Fix attempt: Change theta formula
    
    * Cone fix attempt
    
    * Cone Fix attempt (changing Height and Apothem)
    
    * Cone fixed. Test comments removed from code

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -1,9 +1,11 @@
 #include "generator.hpp"
 #include <cstring>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
+
+static char* PROGRAM_NAME;
 
 #define PARSE_OR_RETURN(a, t, h) \
     if (a == t) {                \
@@ -13,27 +15,27 @@ using namespace std;
 
 void printHelpPlane()
 {
-    cerr << "Help plane" << endl;
+    cerr << "Invalid args, plane requires: side_length" << endl;
 }
 
 void printHelpBox()
 {
-    cerr << "Help box" << endl;
+    cerr << "Invalid args, box requires: x y z [divisions]" << endl;
 }
 
 void printHelpSphere()
 {
-    cerr << "Help sphere" << endl;
+    cerr << "Invalid args, sphere requires: radius slices stacks" << endl;
 }
 
 void printHelpCone()
 {
-    cerr << "Help cone" << endl;
+    cerr << "Invalid args, cone requires: radius height slices stacks" << endl;
 }
 
-void printHelpPage(char* programName)
+void printHelpPage()
 {
-    cerr << "Usage: " << programName << " fileName Option [Params ...]" << endl
+    cerr << "Usage: " << PROGRAM_NAME << " fileName Option [Params ...]" << endl
          << "Options:" << endl
          << "\tplane" << endl
          << "\tbox" << endl
@@ -42,11 +44,11 @@ void printHelpPage(char* programName)
          << "\nUse --help Option to learn what params to pass to the options" << endl;
 }
 
-void write_to_file(char *fileName, vector<Point>* points)
+void write_to_file(char* fileName, vector<Point*>* points)
 {
     ofstream file(fileName);
-    for(vector<Point>::const_iterator it = points->begin(); it != points->end(); ++it){
-        file << (*it).to_string() << endl;
+    for (vector<Point*>::const_iterator it = points->begin(); it != points->end(); ++it) {
+        file << (*it)->to_string() << endl;
     }
 }
 
@@ -62,7 +64,7 @@ bool parse_plane(int argc, char** args, double* side_length)
     return true;
 }
 
-bool parse_box(int argc, char** args, double* x, double* y, double* z)
+bool parse_box(int argc, char** args, double* x, double* y, double* z, int* divisions)
 {
     if (argc < 3) {
         printHelpPlane();
@@ -74,7 +76,11 @@ bool parse_box(int argc, char** args, double* x, double* y, double* z)
     *y = strtod(args[1], &test);
     PARSE_OR_RETURN(args[1], test, printHelpPlane)
     *z = strtod(args[2], &test);
-    PARSE_OR_RETURN(args[2], test, printHelpPlane)
+    PARSE_OR_RETURN(args[3], test, printHelpPlane)
+    *divisions = strtod(args[3], &test);
+    if (test == args[3]) {
+        *divisions = 1;
+    }
     return true;
 }
 
@@ -94,29 +100,32 @@ bool parse_sphere(int argc, char** args, double* radius, double* slices, double*
     return true;
 }
 
-bool parse_cone(int argc, char** args, double* radius, double* slices, double* stacks)
+bool parse_cone(int argc, char** args, double* radius, double* height, int* slices, int* stacks)
 {
-    if (argc < 3) {
+    if (argc < 4) {
         printHelpCone();
         return 1;
     }
     char* test;
-    *radius = strtod(args[2], &test);
+    *radius = strtod(args[0], &test);
+    PARSE_OR_RETURN(args[0], test, printHelpCone);
+    *height = strtod(args[1], &test);
+    PARSE_OR_RETURN(args[1], test, printHelpCone);
+    *slices = strtol(args[2], &test, 10);
     PARSE_OR_RETURN(args[2], test, printHelpCone);
-    *slices = strtod(args[3], &test);
+    *stacks = strtol(args[3], &test, 10);
     PARSE_OR_RETURN(args[3], test, printHelpCone);
-    *stacks = strtod(args[4], &test);
-    PARSE_OR_RETURN(args[4], test, printHelpCone);
     return true;
 }
 
 int main(int argc, char** argv)
 {
+    PROGRAM_NAME = argv[0];
     if (argc < 2) {
-        printHelpPage(argv[0]);
+        printHelpPage();
         return 1;
     }
-    vector<Point> points;
+    vector<Point*> points;
     if (!strcmp("plane", argv[2])) {
         double side_length;
         if (parse_plane(argc - 3, argv + 3, &side_length)) {
@@ -126,8 +135,9 @@ int main(int argc, char** argv)
         }
     } else if (!strcmp("box", argv[2])) {
         double x, y, z;
-        if (parse_box(argc - 3, argv + 3, &x, &y, &z)) {
-            points = draw_box(x, y, z);
+        int divisions;
+        if (parse_box(argc - 3, argv + 3, &x, &y, &z, &divisions)) {
+            points = draw_box(x, y, z, divisions);
         } else {
             return 1;
         }
@@ -139,14 +149,15 @@ int main(int argc, char** argv)
             return 1;
         }
     } else if (!strcmp("cone", argv[2])) {
-        double radius = 0, slices = 0, stacks = 0;
-        if (parse_cone(argc - 3, argv + 3, &radius, &slices, &stacks)) {
-            points = draw_cone(radius, slices, stacks);
+        double radius = 0, height = 0;
+        int slices = 0, stacks = 0;
+        if (parse_cone(argc - 3, argv + 3, &radius, &height, &slices, &stacks)) {
+            points = draw_cone(radius, height, slices, stacks);
         } else {
             return 1;
         }
     } else {
-        printHelpPage(argv[0]);
+        printHelpPage();
         return 1;
     }
     write_to_file(argv[1], &points);
