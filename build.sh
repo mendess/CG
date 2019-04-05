b9c4dc7222eee03f680f1272c7cf307a1b1f5658
Mendess2526 <pedro.mendes.26@gmail.com>
2019-04-05 10:56:44 +0100

    Change buildsh

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@ debug_build="target/debug"
 release_build="target/release"
 mkdir -p $debug_build
 mkdir -p $release_build
-cmake -DCMAKE_BUILD_TYPE=Debug -S src -B $debug_build || exit 1
-cmake -DCMAKE_BUILD_TYPE=Releas -S src -B $release_build || exit 1
-cd $release_build || exit 1
-make || exit 2
-cd ../.. || exit 2
-cd $debug_build || exit 1
-make || exit 3
+if [ "$1" == "-g" ]
+then
+    cmake -Wno-dev -DCMAKE_BUILD_TYPE=Debug -S src -B $debug_build || exit 1
+    cd $debug_build || exit 1
+    make || exit 3
+else
+    cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release -S src -B $release_build || exit 1
+    cd $release_build || exit 1
+    make || exit 2
+fi
