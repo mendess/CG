9817d37756cdc79383f89314e10614a2eb0a6eba
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-04 22:24:44 +0100

    Refactor cylinder

--- src/generator/cylinder.cpp
+++ src/generator/cylinder.cpp
@@ -25,27 +25,28 @@ std::vector<Point> Cylinder::draw() const
     const double top_base = base + height;
     for (int i = 0; i < slices; i++) {
         float d_alpha = alpha + _alpha;
-        double a[3] = { radius * sin(d_alpha), base, radius * cos(d_alpha) };
-        double b[3] = { 0.0, base, 0.0 };
-        double c[3] = { radius * sin(alpha), base, radius * cos(alpha) };
-        double al[3] = { radius * sin(d_alpha), top_base, radius * cos(d_alpha) };
-        double bl[3] = { 0.0, top_base, 0.0 };
-        double cl[3] = { radius * sin(alpha), top_base, radius * cos(alpha) };
+        Point a(radius * sin(d_alpha), base, radius * cos(d_alpha));
+        Point b(0.0, base, 0.0);
+        Point c(radius * sin(alpha), base, radius * cos(alpha));
+        Point al(radius * sin(d_alpha), top_base, radius * cos(d_alpha));
+        Point bl(0.0, top_base, 0.0);
+        Point cl(radius * sin(alpha), top_base, radius * cos(alpha));
         // BASE
-        coords.push_back(Point(c[0], c[1], c[2]));
-        coords.push_back(Point(b[0], b[1], b[2]));
-        coords.push_back(Point(a[0], a[1], a[2]));
+        coords.push_back(c);
+        coords.push_back(b);
+        coords.push_back(a);
         // TOP
-        coords.push_back(Point(al[0], al[1], al[2]));
-        coords.push_back(Point(bl[0], bl[1], bl[2]));
-        coords.push_back(Point(cl[0], cl[1], cl[2]));
+        coords.push_back(al);
+        coords.push_back(bl);
+        coords.push_back(cl);
         // SIDE
-        coords.push_back(Point(a[0], a[1], a[2]));
-        coords.push_back(Point(al[0], al[1], al[2]));
-        coords.push_back(Point(c[0], c[1], c[2]));
-        coords.push_back(Point(c[0], c[1], c[2]));
-        coords.push_back(Point(al[0], al[1], al[2]));
-        coords.push_back(Point(cl[0], cl[1], cl[2]));
+        coords.push_back(a);
+        coords.push_back(al);
+        coords.push_back(c);
+
+        coords.push_back(c);
+        coords.push_back(al);
+        coords.push_back(cl);
         alpha += _alpha;
     }
 
