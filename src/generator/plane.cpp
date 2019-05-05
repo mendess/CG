969bb4f81df76837c12e988e28549f77d183201f
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-06 00:14:04 +0100

    Plane with normals and texture coords

--- src/generator/plane.cpp
+++ src/generator/plane.cpp
@@ -18,10 +18,11 @@ std::vector<Point> Plane::draw() const
     vector<Point> coordsPlane;
     float axis = side_length / 2;
 
-    Point p0(-axis, 0, -axis);
-    Point p1(-axis, 0, axis);
-    Point p2(axis, 0, -axis);
-    Point p3(axis, 0, axis);
+    Vector normal(0, 1, 0);
+    Point p0(-axis, 0, -axis, normal, 0, 0);
+    Point p1(-axis, 0, axis, normal, 0, 1);
+    Point p2(axis, 0, -axis, normal, 1, 0);
+    Point p3(axis, 0, axis, normal, 1, 1);
     //first triangle
     coordsPlane.push_back(p3);
     coordsPlane.push_back(p0);
