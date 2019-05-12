5c18579629b62121470e7b6ef3b603b61b84afb4
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-12 14:55:12 +0100

    Reorganize models

--- scripts/generate_unit_models.sh
+++ scripts/generate_unit_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=( scenes/box.3d scenes/cone.3d scenes/plane.3d scenes/sphere.3d  scenes/torus.3d )
+files=( models/box.3d models/cone.3d models/plane.3d models/sphere.3d  models/torus.3d )
 args=( "1 1 1 1"        "1 1 20 20"    "1"             "1 50 50"         "0.545 2.327 50 30")
 for ((i = 0; i < ${#files[@]}; i++))
 do
@@ -10,10 +10,10 @@ do
     ./target/release/generator/generator "${files[$i]}" "$(basename "${files[$i]}" .3d)" ${args[$i]}
 done
 echo -en "\033[32m"
-echo scenes/piramid.3d cone 1 1 4 30
+echo models/piramid.3d cone 1 1 4 30
 echo -en "\033[0m"
-./target/release/generator/generator scenes/piramid.3d cone 1 1 4 30
+./target/release/generator/generator models/piramid.3d cone 1 1 4 30
 echo -en "\033[32m"
-echo scenes/teapot.3d patch scenes/teapot.patch 5
-./target/release/generator/generator scenes/teapot.3d patch scenes/teapot.patch 5
+echo models/teapot.3d patch models/teapot.patch 5
+./target/release/generator/generator models/teapot.3d patch models/teapot.patch 5
 echo -en "\033[0m"
