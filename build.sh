b1b58775aabe39e20f303a871d82cd0c2af5db84
Mendess2526 <pedro.mendes.26@gmail.com>
2019-06-04 18:10:38 +0100

    fix build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ debug_build="target/debug"
 release_build="target/release"
 mkdir -p $debug_build
 mkdir -p $release_build
-if [ "$1" == "-g" ]
+if [ "$1" = "-g" ]
 then
     cmake -Wno-dev -DCMAKE_BUILD_TYPE=Debug -S src -B $debug_build || exit 1
     cd $debug_build || exit 1
